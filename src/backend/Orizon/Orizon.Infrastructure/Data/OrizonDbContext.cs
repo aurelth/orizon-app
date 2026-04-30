55a2675b6ccfde2557d8e2e1bad0021aedf6e639
Aurel Lossou <aurelth@icloud.com>
2026-04-30 15:11:51 -0300

    feat: adicionado DbContext, AppIdentityUser e configurações do EF Core

---
+++ src/backend/Orizon/Orizon.Infrastructure/Data/OrizonDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Orizon.Domain.Entities;
+using Orizon.Infrastructure.Identity;
+
+namespace Orizon.Infrastructure.Data;
+
+public class OrizonDbContext : IdentityDbContext<AppIdentityUser>
+{
+    public OrizonDbContext(DbContextOptions<OrizonDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<BriefingEntry> BriefingEntries => Set<BriefingEntry>();
+    public DbSet<TrelloBoardConfig> TrelloBoardConfigs => Set<TrelloBoardConfig>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {      
+        base.OnModelCreating(builder);
+     
+        builder.ApplyConfigurationsFromAssembly(
+            typeof(OrizonDbContext).Assembly);
+        
+        builder.Entity<AppIdentityUser>().ToTable("users");
+    }
+}
